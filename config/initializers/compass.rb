c37f4e3ec6166d8863e01f825ef2ff9f784de00d
Travis Haynes <travis.j.haynes@gmail.com>
2011-06-24 09:07:45 -0700

    Added some configuration to get Compass to play nicely with Heroku.

---
+++ config/initializers/compass.rb
@@ -0,0 +1,15 @@
+# configuration to get Compass to play nicely with Heroku
+# makes sure that tmp/stylesheets exists when the app starts running and that
+# the stylesheets are served as static assets.
+
+require 'fileutils'
+FileUtils.mkdir_p(Rails.root.join("tmp", "stylesheets"))
+
+Compass::AppIntegration::Rails.initialize!
+
+Rails.configuration.middleware.delete('Sass::Plugin::Rack')
+Rails.configuration.middleware.insert_before('Rack::Sendfile', 'Sass::Plugin::Rack')
+
+Rails.configuration.middleware.insert_before('Rack::Sendfile', 'Rack::Static',
+    :urls => ['/stylesheets'],
+    :root => "#{Rails.root}/tmp")
